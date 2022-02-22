5b21d401a66b65f72a58e67600de2beef06f9b35
rosemolthomasparava7 <77224849+rosemolthomasparava7@users.noreply.github.com>
2022-02-22 15:47:33 +0530

    Add files via upload

---
+++ Context menu/MainActivityContextMenu.java
@@ -0,0 +1,50 @@
+package com.example.sjcet.myapplicationcontextmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivityContextMenu extends AppCompatActivity {
+    ListView ListView;
+    String contacts[]={"Ajay","Sachin","Sumit","Tarun","Yogesh"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_context_menu);
+        ListView=(ListView)findViewById(R.id.ListView);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contacts);
+        ListView.setAdapter(adapter);
+        registerForContextMenu(ListView);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        menu.setHeaderTitle("Select The Action");
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.call){
+            Toast.makeText(getApplicationContext(),"calling code",Toast.LENGTH_LONG).show();
+        }
+        else if(item.getItemId()==R.id.sms){
+            Toast.makeText(getApplicationContext(),"sending sms code",Toast.LENGTH_LONG).show();
+        }else{
+            return false;
+        }
+        return true;
+    }
+}
